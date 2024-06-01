7d22e2e03eb4f8a9bc946109d4d47d0d660a3f45
BMWdejf <sablatura.david@gmail.com>
2024-06-01 09:41:38 +0200

    Add serializers and modify views

--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+
 from .models import Project
+from .serializers import ProjectSerializer
 
 def index(request):
     return HttpResponse("Hello, world. You're at the api index.")
@@ -11,8 +14,13 @@ class ProjectViewSet(viewsets.ModelViewSet):
     permission_classes = [
         permissions.AllowAny
     ]
+    serializer_class = ProjectSerializer
+
     def list(self, request):
-        pass
+        queryset = self.queryset
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
     def create(self, request):
         pass
     def retrieve(self, request):
